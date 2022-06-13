c5e5406a03fdf5b36fdec6140e9045b70198eb24
RealBlueta <jejojora2@gmail.com>
2022-06-13 08:49:44 -0400

    Start adding lexums

--- src/lexer.ts
+++ src/lexer.ts
@@ -5,6 +5,7 @@ export default class Lexer {
     private row: number;
     private col: number;
 
+    // Lexer Stuff
     constructor(private src: string, public file_name: string) {
         this.cursor = 0;
         this.row = 0;
@@ -21,7 +22,38 @@ export default class Lexer {
 
     run(): Token[] { 
         const tokens: Token[] = []; 
+        
         tokens.push(new Token(TokenType.EOF, this.position()));
         return tokens; 
+    }
+
+    // Lexing Functions (Data)
+    current(): string | undefined {
+        return this.src[this.cursor];
+    }
+
+    peek(num: number): string | undefined {
+        return this.src[this.cursor + num];
+    }
+
+    advance(amount: number = 1): void {
+        this.col += amount;
+        this.cursor += amount;
+    }
+
+    // Lexing Functions (Lexums)
+    consume_space(): void {
+        this.advance();
+    }
+
+    consume_newline(): void {
+        this.cursor += 1;
+        this.col = 0;
+        this.row += 1;
+    }
+
+    consume_tab(): void {
+        this.cursor += 1;
+        this.col += 4;
     }
 }
