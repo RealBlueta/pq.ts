a39a7e39492aa95d7b5b73d8c32dfb54bcc9f65e
RealBlueta <jejojora2@gmail.com>
2022-06-13 17:38:11 -0400

    Add lexer to parser constructor

--- src/parser.ts
+++ src/parser.ts
@@ -1,11 +1,17 @@
+import Lexer from "./lexer";
 import { Node } from "./expression";
+import { Token } from "./token";
 
 export default class Parser {
-    constructor() {}
+    private tokens: Token[];
+    
+    constructor(lexer: Lexer) {
+        this.tokens = lexer.run();
+    }
 
     run(): Node[] {
         const ast: Node[] = [];
-
+        
         return ast;
     }
 }
