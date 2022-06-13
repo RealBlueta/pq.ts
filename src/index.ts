a6897c9cc478b8c365d824a913aae18589e7ef8e
RealBlueta <jejojora2@gmail.com>
2022-06-13 08:37:21 -0400

    Check if file ends in .pq

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,13 @@ process.exit((
         const args: string[] = argv.slice(2);
         if (args.length < 1) return usage();
         try {
-            const contents: string = readFileSync(args[0]).toString();
-            const tokens = new Lexer(contents, args[0]).run();
+            const file_name = args[0];
+            if (!file_name.endsWith('.pq')) {
+                console.log('You can only run files that end in \'.pq\'.');
+                return 1;
+            }
+            const contents: string = readFileSync(file_name).toString();
+            const tokens = new Lexer(contents, file_name).run();
             for (let token of tokens) {
                 console.log(token);
             }
